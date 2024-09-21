37288c80e88d738e7d9c0bca013cca8a494e8046
caesarrimskiyyyyyyyyyyyy <caesarrimskiy@gmail.com>
2024-09-21 17:09:44 +0300

    init

---
+++ src/test/java/ru/javamentor/SpringSecurity/ApplicationTests.java
@@ -0,0 +1,11 @@
+package ru.javamentor.SpringSecurity;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+    @Test
+    void contextLoads() {
+    }
+}
