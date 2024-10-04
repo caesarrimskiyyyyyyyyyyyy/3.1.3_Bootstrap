6346c5df223a0c90ad0888ff2e373efc94038eb2
caesarrimskiyyyyyyyyyyyy <caesarrimskiy@gmail.com>
2024-10-05 00:09:43 +0300

    fix exception

--- src/main/java/ru/javamentor/SpringSecurity/services/UserServiceImpl.java
+++ src/main/java/ru/javamentor/SpringSecurity/services/UserServiceImpl.java
@@ -1,8 +1,8 @@
 package ru.javamentor.SpringSecurity.services;
 
+import jakarta.persistence.EntityNotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Lazy;
-import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
@@ -27,7 +27,7 @@ public class UserServiceImpl implements UserService {
     @Override
     public User getUserByUsername(String username) {
         return userRepository.findUserByUsername(username)
-                .orElseThrow(() -> new UsernameNotFoundException(username + " not found"));
+                .orElseThrow(() -> new EntityNotFoundException(username + " not found"));
     }
 
     @Override
